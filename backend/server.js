b7decfb8b95068b7888f9b9e1c0287101a693318
Thushantha Sanju <thushanthasanju8@gmail.com>
2022-09-07 08:17:43 +0530

    env

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,13 @@
 import dotenv from "dotenv";
 import express from "express";
+import cors from "cors";
 import mongoose from "mongoose";
 import subscribersRouter from "./routes/subscribers.js";
 
 const PORT = process.env.PORT || "5000";
 dotenv.config();
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 mongoose.connect(process.env.DATABASE_URL, {
