dde1b2d8e5afcf0388a46dfcfd1ee0cd8bdd0458
Keshara Bandara <86250931+KesharaBandara@users.noreply.github.com>
2022-10-13 21:42:57 +0530

    Update header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,24 +6,19 @@ import Home from "./layouts/Home";
 import { Routes, Route } from "react-router-dom";
 import Packages from "./pages/Packages";
 import AddPackage from "./components/AddPackage";
-import LiveBoardView from "./pages/LiveBoard";
-import AddLiveBoard from "./pages/AddLiveBoard";
-import AdminBooking from "./pages/Bookings";
 import "bootstrap/dist/css/bootstrap.min.css";
 import CreateResort from "./components/createResort";
 import ShowResort from "./pages/showResorts";
 import UpdateResort from "./components/updateResort";
 import UpdatePackage from "./components/UpdatePackage";
 import AdminDash from "./layouts/AdminDash";
-import LiveBoardCusView from "./pages/LiveaboardCusView";
-import AddLBbookings from "./pages/AddLBbookings";
+import { PackageDetails } from "./components/PackageDetails";
 
 function App() {
   return (
     <div>
       <Header />
 
-     
       <section>
         {localStorage.getItem("token") ? (
           <Routes>
@@ -33,13 +28,9 @@ function App() {
             <Route path="/packages" element={<Packages />} />
             <Route path="/addpackage" element={<AddPackage />} />
             <Route path="/admindashboard" element={<AdminDash />} />
+            <Route path="/packagedetails" element={<PackageDetails />} />
             <Route path="/resorts" element={<ShowResort />} />
             <Route path="/create-resorts" element={<CreateResort />} />
-            <Route path="/liveBoardView" element={<LiveBoardView />} />
-            <Route path="/addliveboard" element={<AddLiveBoard />} />
-            <Route path="/bookingsadmin" element={<AdminBooking />} />
-            <Route path="/LiveBoardCusView" element={<LiveBoardCusView/>} />
-            <Route path="/AddLBbookings/:id" exact={false} element={<AddLBbookings/>}/>
             <Route
               path="/update-resorts/:id"
               exact={false}
@@ -53,7 +44,8 @@ function App() {
           </Routes>
         ) : (
           <Routes>
-            <Route path="/" element={<SignIn />} />
+            <Route path="/signin" element={<SignIn />} />
+            <Route path="/signup" element={<SignUp />} />
           </Routes>
         )}
       </section>
--- frontend/src/layouts/Header.jsx
+++ frontend/src/layouts/Header.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { AppBar, Button, Tab, Tabs, Toolbar, Typography } from '@mui/material'
 import ScubaDivingIcon from '@mui/icons-material/ScubaDiving';
 import { useState } from 'react';
@@ -10,8 +10,36 @@ import { useNavigate } from 'react-router-dom';
 const Header = () => {
     const navigate = useNavigate();
     const [value, setValue] = useState();
+    const [index, setindex] = useState(0);
+
+    useEffect(() => {
+
+        switch (window.location.pathname) {
+            case "/signin":
+                setindex(1)
+                break;
+            case "/signup":
+                setindex(2)
+                break;
+            case "/":
+                setindex(3)
+                break;
 
 
+            default:
+                break;
+        }
+    }, [])
+
+    let token = localStorage.getItem("token");
+    let role = localStorage.getItem("role");
+    const userLogout = () => {
+        localStorage.removeItem("token");
+        localStorage.removeItem("role");
+        navigate('/signin ')
+    }
+
+    console.log("user", role);
 
 
 
@@ -24,19 +52,34 @@ const Header = () => {
                     <Typography >{'\u00A0'}{'\u00A0'} Diving App{'\u00A0'}{'\u00A0'}</Typography>
                     <Tabs sx={{ marginRight: 'auto', "& button:Mui-selected": { backgroundColor: "red" } }} textColor="inherit" value={value} onChange={(e, value) => setValue(value)} indicatorColor='secondary'>
 
+                        {role === "User" ? <></> :
+                            <>
+                                {(index === 1 || index === 2 || index === 3) ? <></> : <> <Tab label="Live Boards" />
+                                    <Tab label="Diving Resorts" href="/resorts" />
+                                    <Tab label="Diving Packages" href="/packages" />
+                                    <Tab label="Snorkeling" /></>}
 
 
-                        <Tab label="Live Boards" href="/liveBoardView" />
-                        <Tab label="Diving Resorts" href="/resorts" />
-                        <Tab label="Diving Packages" href="/packages" />
-                        <Tab label="Snorkeling" href="/LiveBoardCusView" />
+                            </>
+                        }
+
 
 
 
                     </Tabs>
-                    <Button variant='contained' sx={{ marginLeft: 'auto' }} onClick={() => navigate('/signin')}>Login</Button>
-                    <Button variant='contained' sx={{ marginLeft: '10px ' }} onClick={() => navigate('/signup')}>Register</Button>
 
+                    {token ? <Button variant='contained' sx={{ marginLeft: 'auto' }} onClick={() => { userLogout() }}>
+                        Logout
+                    </Button> : <>
+                        {index === 1 ?
+                            <Button variant='contained' sx={{ marginLeft: '10px ' }} href="/signup">Register</Button>
+
+                            :
+                            <Button variant='contained' sx={{ marginLeft: 'auto' }} href="/signin">Login</Button>
+                        }
+
+
+                    </>}
 
 
 
